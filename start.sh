0be1d311387aa9415e568551ec45eaddd5c3f79d
Ahmad Mohammad Abdel-Hafeez <63889688+Ahmad3bdelHafeez@users.noreply.github.com>
2026-02-17 18:27:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PLAYWRIGHT_MCP_ALLOWED_ORIGINS="*"
 export PLAYWRIGHT_MCP_HOST="0.0.0.0"
-exec npx @playwright/mcp@mcp@0.0.14 \
+exec npx @playwright/mcp@0.0.14 \
   --port 10000 \
   --host 0.0.0.0 \
   --allowed-origins "*" \
